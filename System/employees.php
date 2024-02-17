0bb4ba8270d663d4e09857d8ce722e8e5efa397e
Sagar <95471160+sagar01101100@users.noreply.github.com>
2024-02-17 20:12:32 +0530

    Update employees.php

--- System/employees.php
+++ System/employees.php
@@ -38,7 +38,7 @@ $page = 1;
     <meta property="og:image:alt" content="Bwire Jobs" />
     <meta property="og:description" content="Online Job Management / Job Portal" />
 
-	<link rel="shortcut icon" href="images/ico/iip_logo.png">
+	<link rel="shortcut icon" href="images/ico/iitp_logo.png">
 	
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">	
 	<link href="css/animate.css" rel="stylesheet">
