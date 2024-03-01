cd24386f6aa7376487819a7292c024281350ae88
sagar01101100 <sagar01101100@gmail.com>
2024-03-01 16:50:56 +0530

    ...

--- System/register.php
+++ System/register.php
@@ -10,7 +10,7 @@ include 'constants/check-login.php';
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>IITP Register</title>
+	<title>Nightingale Jobs - Register</title>
 	<meta name="description" content="Online Job Management / Job Portal" />
 	<meta name="keywords" content="job, work, resume, applicants, application, employee, employer, hire, hiring, human resource management, hr, online job management, company, worker, career, recruiting, recruitment" />
 	<meta name="author" content="BwireSoft">
@@ -23,7 +23,7 @@ include 'constants/check-login.php';
     <meta property="og:image:alt" content="Nightingale Jobs" />
     <meta property="og:description" content="Online Job Management / Job Portal" />
 
-	<link rel="shortcut icon" href="images/ico/iitp_logo.png">
+	<link rel="shortcut icon" href="images/ico/favicon.png">
 
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">	
 	<link href="css/animate.css" rel="stylesheet">
@@ -60,20 +60,31 @@ include 'constants/check-login.php';
 					
 					<div class="logo-wrapper">
 						<div class="logo">
-							<a href="./"><img width  ="50rem" src="images/ico/iitp_logo.png" alt="Logo" /></a>
+							<a href="./"><img src="images/logo.png" alt="Logo" /></a>
 						</div>
 					</div>
 					
 					<div id="navbar" class="navbar-nav-wrapper navbar-arrow">
 					
 						<ul class="nav navbar-nav" id="responsive-menu">
-						<li>
-								<a href="job-list.php">Apply Now</a>
+						
+							<li>
+							
+								<a href="./">Home</a>
+								
+							</li>
+							
+							<li>
+								<a href="job-list.php">Job List</a>
 
 							</li>
 							
 							<li>
-								<a href="employers.php">IITP Faculties</a>
+								<a href="employers.php">Employers</a>
+							</li>
+							
+							<li>
+								<a href="employees.php">Employees</a>
 							</li>
 							
 							<li>
@@ -86,8 +97,8 @@ include 'constants/check-login.php';
 
 					<div class="nav-mini-wrapper">
 						<ul class="nav-mini sign-in">
-						<li><a href="login.php">login</a></li>
-							<li><a href="applicant_register.php">Register</a></li>
+							<li><a  href="login.php">login</a></li>
+							<li><a data-toggle="modal" href="#registerModal">register</a></li>
 						</ul>
 					</div>
 				
@@ -96,6 +107,38 @@ include 'constants/check-login.php';
 				<div id="slicknav-mobile"></div>
 				
 			</nav>
+	
+			<div id="registerModal" class="modal fade login-box-wrapper" tabindex="-1" style="display: none;" data-backdrop="static" data-keyboard="false" data-replace="true">
+			
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title text-center">Create your account for free</h4>
+				</div>
+				
+				<div class="modal-body">
+				
+					<div class="row gap-20">
+					
+						<div class="col-sm-6 col-md-6">
+							<a href="register.php?p=Employer" class="btn btn-facebook btn-block mb-5-xs">Register as Employer</a>
+						</div>
+						<div class="col-sm-6 col-md-6">
+							<a href="register.php?p=Employee" class="btn btn-facebook btn-block mb-5-xs">Register as Employee</a>
+						</div>
+
+					</div>
+				
+				</div>
+				
+				<div class="modal-footer text-center">
+					<button type="button" data-dismiss="modal" class="btn btn-primary btn-inverse">Close</button>
+				</div>
+				
+			</div>
+
+
+
+			
 		</header>
 
 
@@ -163,97 +206,97 @@ include 'constants/check-login.php';
 			</div>
 			<footer class="footer-wrapper">
 			
-			<div class="main-footer">
-			
-				<div class="container">
+				<div class="main-footer">
 				
-					<div class="row">
+					<div class="container">
 					
-						<div class="col-sm-12 col-md-9">
+						<div class="row">
 						
-							<div class="row">
+							<div class="col-sm-12 col-md-9">
 							
-								<div class="col-sm-6 col-md-4">
+								<div class="row">
 								
-									<div class="footer-about-us">
-										<h5 class="footer-title">About IIT Patna Faculty Recruitment</h5>
-										<p>Explore faculty opportunities at IIT Patna through our online job portal.</p>
+									<div class="col-sm-6 col-md-4">
+									
+										<div class="footer-about-us">
+											<h5 class="footer-title">About Nightingale Jobs</h5>
+											<p>Nightingale Jobs is a job portal, online job management system developed by Nathaniel Nkrumah for his project in february 2018.</p>
+										
+										</div>
+
+									</div>
+									
+									<div class="col-sm-6 col-md-5 mt-30-xs">
+										<h5 class="footer-title">Quick Links</h5>
+										<ul class="footer-menu clearfix">
+											<li><a href="./">Home</a></li>
+											<li><a href="job-list.php">Job List</a></li>
+											<li><a href="employers.php">Employers</a></li>
+											<li><a href="employees.php">Employees</a></li>
+											<li><a href="contact.php">Contact Us</a></li>
+											<li><a href="#">Go to top</a></li>
+
+										</ul>
 									
 									</div>
 
 								</div>
-								
-								<div class="col-sm-6 col-md-5 mt-30-xs">
-									<h5 class="footer-title">Quick Links</h5>
-									<ul class="footer-menu clearfix">
-										<li><a href="./">Home</a></li>
-										<li><a href="job-list.php">Faculty Recruitment</a></li>
-										<li><a href="employers.php">Employers</a></li>
-										<li><a href="employees.php">Employees</a></li>
-										<li><a href="contact.php">Contact Us</a></li>
-										<li><a href="#">Go to top</a></li>
 
-									</ul>
+							</div>
+							
+							<div class="col-sm-12 col-md-3 mt-30-sm">
+							
+								<h5 class="footer-title">Nightingale Jobs Contact</h5>
 								
-								</div>
+								<p>Address : Takoradi, School Junction PO.BOX AX40</p>
+								<p>Email : <a href="mailto:nightingale.nath2@gmail.com">nightingale.nath2@gmail.com</a></p>
+								<p>Phone : <a href="tel:+233546607474">+233 546 607 474</a></p>
+								
 
 							</div>
 
-						</div>
-						
-						<div class="col-sm-12 col-md-3 mt-30-sm">
-						
-							<h5 class="footer-title">IIT Patna Contact</h5>
 							
-							<p>Address : Bihta Kanpa Rd, Patna, Dayalpur Daulatpur, Bihar 801106</p>
-							<p>Email : <a href="mailto:iitpatna@gmail.com">iitpatna@gmail.com</a></p>
-							<p>Phone : <a href="tel:+91 989592XXXX">+91 989592XXXX</a></p>
-							
-
 						</div>
-
 						
 					</div>
 					
 				</div>
 				
-			</div>
-			
-			<div class="bottom-footer">
-			
-				<div class="container">
+				<div class="bottom-footer">
 				
-					<div class="row">
+					<div class="container">
 					
-						<div class="col-sm-4 col-md-4">
-				
-							<p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> IIT Patna</p>
-							
-						</div>
+						<div class="row">
 						
-						<div class="col-sm-4 col-md-4">
-						
-							<ul class="bottom-footer-menu">
+							<div class="col-sm-4 col-md-4">
+					
+								<p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> Nightingale Vision Software</p>
 								
-							</ul>
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+							
+								<ul class="bottom-footer-menu">
+									<li><a >Developed by Nathaniel Nkrumah</a></li>
+								</ul>
+							
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+								<ul class="bottom-footer-menu for-social">
+									<li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+									<li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+									<li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+								</ul>
+							</div>
 						
 						</div>
-						
-						<div class="col-sm-4 col-md-4">
-							<ul class="bottom-footer-menu for-social">
-								<li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
-								<li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
-								<li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
-							</ul>
-						</div>
-					
-					</div>
 
+					</div>
+					
 				</div>
-				
-			</div>
-		
-		</footer>
+			
+			</footer>
 			
 		</div>
 
