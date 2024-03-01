ddaeb61d5d5d9a49f42781f72490fb7a8bb6451e
sagar01101100 <sagar01101100@gmail.com>
2024-03-01 15:37:36 +0530

    ...

--- System/constants/db_config.php
+++ System/constants/db_config.php
@@ -1,6 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12678896";
+$password = "qtjwTG66mK";
 $dbname = "job_portal";
 ?>
+
+
+<!-- Host: sql12.freesqldatabase.com
+Database name: sql12678896
+Database user: sql12678896
+Database password: qtjwTG66mK
+Port number: 3306 -->
